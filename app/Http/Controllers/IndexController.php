3a737042153c9de074c98b26e9120790dc3f2688
Norbinations <vladik221297@mail.ru>
2018-05-16 08:26:58 +0300

    vladweb

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,18 +4,51 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-//use App\Http\Requests;
+use App\Article;
 
-class IndexController extends Controller
-{
-    //
-    public function index() {
-    	
-    	$header = 'Hello World!!!';
-    	$message = 'This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.';
-		
-		return view('page')->with(['message'=>$message,'header' => $header]);
-		
-	}
-    
+class IndexController extends Controller {
+
+    protected $message;
+    protected $PS;
+
+    public function __construct() {
+      $this->message = "WELCOME";
+      $this->PS = "To my first page at PHP with laravel";
+    }
+
+    public function index () {
+      $articles = Article::select(['id','title','descr'])->get();
+      return view('welcome')->with(['message' => $this->message,
+                                    'PS' => $this->PS,
+                                    'articles' => $articles]);
+    }
+
+    public function show ($id) {
+      $article = Article::select(['id','title','text'])->where('id', $id)->first();
+      return view('page')->with(['article' => $article]);
+    }
+
+    public function add () {
+      return view('add-content');
+    }
+
+    public function store (Request $request) {
+      $this->validate($request, [
+
+        'title' => 'required|max:100',
+        'descr' => 'required|max:255',
+        'text' => 'required'
+
+      ]);
+
+      $data = $request->all();
+
+      $article = new Article;
+      $article->fill($data);
+
+      $article->save();
+
+      return redirect('/');
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,25 +5,18 @@
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-// GET  POST
-// PUT DELETE UPDATE
-
-//page 
-/*Route::get('/', function () {
-	
-	// View
-    return view('welcome');
-    
-});*/
-
-Route::get('/', 'IndexController@index');
-
-
-
-
+Route::get('/', 'IndexController@index')->name('index');
+Route::get('article/{id}', 'IndexController@show')->name('articleShow');
+Route::get('page/add', 'IndexController@add')->name('articleAdd');
+Route::post('page/add', 'IndexController@store')->name('articleStore');
+Route::delete('page/delete/{article}', function (\App\Article $article) {
+  //$article_tmp =  \App\Article::where('id',$article)->first();
+  $article->delete();
+  return redirect('/');
+})->name('articleDelete');
